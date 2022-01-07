281b87661880d896f80b7b4dd84e09f8dfd8124a
FHAACHEN-PHYSICS-PROGRAMMING-LAB <94440162+FHAACHEN-PHYSICS-PROGRAMMING-LAB@users.noreply.github.com>
2022-01-07 12:32:24 +0100

    Update Erzwungene_Drehschwingung.py

--- Erzwungene_Drehschwingung.py
+++ Erzwungene_Drehschwingung.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-df=pd.read_csv("ErzwungeneDrehschwingung_02_Gr27.csv",sep=';')
+df=pd.read_csv("ErzwungeneDrehschwingung.csv",sep=';')
 df1=df.dropna() #Dataframe without NaN
 
 df1 = df1.replace(',','.', regex=True)
@@ -25,7 +25,7 @@ Amplitude=np.round(Amplitude,3)
 plt.plot(Kreisfrequenz,Amplitude,label='Messdaten')
 plt.xlabel('Kreisfrequenz in rad/s')
 plt.ylabel('Amplitude in rad')
-plt.xticks(np.arange(0, 50, step=5))
+plt.xticks(np.arange(1,6, step=.5))
 
 #Estimating 
 delta=0.05
@@ -37,23 +37,23 @@ def Testfunktion(Kreisfrequenz,N,Kreisfrequenz0,delta,Off):
     return N/(np.sqrt((Kreisfrequenz0**2-Kreisfrequenz**2)**2 +4*delta**2*Kreisfrequenz**2))+Off
 popt,pcov=curve_fit(Testfunktion,Kreisfrequenz,Amplitude,[N,Kreisfrequenz0,delta,Off])
 
-print(popt)
+#print(popt)
 
 #Bestimme das R^2=1-(Residual Sum of Squares)/(Total Sum of Squars). Das R^2 gibt an, wie gut der Fit bzw. unsere Funktion zu den Daten passt
 residuals=Amplitude -Testfunktion(Kreisfrequenz,*popt) #Bestimme die Residuen, die Abweichung von der Amplitude zur Fitfunktion
 Res_square=np.sum(residuals**2)#Quadrat der Residuen
 ss_tot=np.sum((Amplitude -np.mean(Amplitude))**2)# Bestimmte die totale Quadratsumme
 R2=1-Res_square/ss_tot #Bestimmte das R^2
-print('Das Bestimmtheitsmass R^2 ist ={}'.format(R2))
+print('Das Bestimmtheitsmass R^2 ist ={}'.format(round(R2,3)))
 
 
 #Ausgabe der Parameter mit Fehler in der Konsole
-print('Die Fitparameter für das N=Drehmoment/Traegheitsmoment ist N = {} +- {}'.format(round(popt[0],2),round(np.sqrt(pcov[0][0]),2)))
-print('Die Fitparameter für die Resonanzfrequenz ist w0 = {} +- {}'.format(round(popt[1],2),round(np.sqrt(pcov[1][1]),2)))
-print('Die Fitparameter für die Dämpfung ist delta ={} +- {}'.format(round(popt[2],4),round(np.sqrt(pcov[2][2]),4)))
-print('Die Fitparameter für den Offset der Amplitude ist Offset = {} +- {}'.format(round(popt[3],2),round(np.sqrt(pcov[3][3]),2)))
+print('Die Fitparameter für das N=Drehmoment/Traegheitsmoment ist N = ({} +- {}) 1/s^2'.format(round(popt[0],2),round(np.sqrt(pcov[0][0]),2)))
+print('Die Fitparameter für die Resonanzfrequenz ist w0 = ({} +- {}) rad/s'.format(round(popt[1],3),round(np.sqrt(pcov[1][1]),3)))
+print('Die Fitparameter für die Dämpfung ist delta =({} +- {}) 1/s'.format(round(popt[2],4),round(np.sqrt(pcov[2][2]),4)))
+print('Die Fitparameter für den Offset der Amplitude ist Offset = ({} +- {}) rad'.format(round(popt[3],2),round(np.sqrt(pcov[3][3]),2)))
 
-print('Das R^2 ist {}'.format(R2))  #Gebe das R^2 in der Konsole aus.
+print('Das R^2 ist {}'.format(round(R2,3))) #Gebe das R^2 in der Konsole aus.
 
 
 
